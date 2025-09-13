531e8f43bffbd526a96adff14693390e4b4cc130
K <k.rev.urtica@gmail.com>
2025-09-13 19:07:19 +0900

    fix: update repo link

--- app/components/shared/GithubButton.vue
+++ app/components/shared/GithubButton.vue
@@ -1,7 +1,7 @@
 <template>
   <UTooltip text="GitHub Repository" arrow>
     <UButton
-      to="https://github.com/k-urtica/border-radius-pro"
+      to="https://github.com/k-urtica/gradient-text-pro"
       target="_blank"
       variant="outline"
       icon="i-lucide-github"
